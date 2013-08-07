d39bf60a45417b5385918e39edd5f0d1780f0a50
Matt Parlette <matthew.parlette@gmail.com>
2013-08-07 13:20:08 -0400

    Created rails site with Gemfile from journal.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rak::Application.config.secret_key_base = 'e950367f4d335c3eb945f454afc5c4132dbb9e4f131f722b35cfb05c0337409a9f453a8db6cb4bf9be6df713b7c203fa6256e2f2fa9e7ab05b88aa3f19fc01d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rak::Application.config.session_store :cookie_store, key: '_rak_session'
