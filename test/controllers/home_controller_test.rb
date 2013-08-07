f03f89d3bdb4f9ad538be1cc9b9175c7bbc3fe66
Matt Parlette <matthew.parlette@gmail.com>
2013-08-07 17:08:41 -0400

    Added tests for user login and logout.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "redirect when not logged in" do
+    get :index
+    assert_response :redirect
+  end
+  
+  test "login user" do
+    sign_in User.first
     get :index
     assert_response :success
+  end
+  
+  test "logout user" do
+    sign_out User.first
+    get :index
+    assert_response :redirect
   end
 
 end
