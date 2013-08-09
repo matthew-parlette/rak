43bfaeafa90c9ac9ddeb89ad64878e89afb5f8ed
Matt Parlette <matthew.parlette@gmail.com>
2013-08-09 13:15:33 -0400

    Added create and new methods.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -18,11 +18,33 @@ class RelationshipsController < ApplicationController
     end
   end
   
+  def new
+    @relationship = Relationship.new
+  end
+  
+  def create
+    @relationship = Relationship.new(rel_params)
+    @relationship.user = current_user
+    
+    respond_to do |format|
+      if @relationship.save
+        format.html { render json: @relationship, notice: 'Task was successfully created.' }
+        format.js   {}
+        format.json { render json: @relationship, status: :created, location: @task }
+      else
+        format.html { render json: @relationship.errors, status: :unprocessable_entity }
+        format.json { render json: @relationship.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   private
     def get_relationships
-      @relationships = Relationship.where(:user_id => current_user)
+      @relationships = Relationship.where(:user_id => current_user[:id])
     end
     
-    
+    def rel_params
+      params.require(:relationship).permit(:title,:name,:description)
+    end
   
 end
