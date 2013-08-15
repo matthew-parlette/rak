80fd4c82ac6900979a41b472825a254a539d9327
Matt Parlette <matthew.parlette@gmail.com>
2013-08-15 14:22:32 -0400

    Wrote a few tests, needs more later.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,4 @@
 class Relationship < ActiveRecord::Base
   belongs_to :user
+  validates :title, presence: true
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without title" do
+    r = Relationship.new
+    assert !r.save, "Saved the relationship without a title"
+  end
+  
+  test "add a new relationship" do
+    r = Relationship.new
+    r['title'] = "A new relationship"
+    r['name'] = "My relationship name"
+    r['user_id'] = 1
+    assert r.save, "New relationship could not be saved"
+  end
+  
+  test "destroy a relationship" do
+    assert Relationship.last.destroy, "Last Relationship was not destroyed"
+  end
 end
