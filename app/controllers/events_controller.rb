2f881ac2d88392438da7b3deb7b3810c2a108101
Matt Parlette <matthew.parlette@gmail.com>
2013-08-28 13:59:47 -0400

    Implemented new and create.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,66 @@
 class EventsController < ApplicationController
+  before_filter :get_event, :only => [:show,:edit,:update,:destroy]
+  before_filter :get_parent_idea, :only => [:show, :create, :new, :destroy]
+  before_filter :get_parent_relationship, :only => [:destroy]
+  
   def show
   end
 
   def new
+    @event = Event.new
+    @event.date = Date.today
+    if @idea
+      logger.info "idea is valid"
+      @event.idea_id = @idea.id
+    end
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    @event.idea_id = @idea.id
+    
+    respond_to do |format|
+      if @event.save
+        flash[:notice] = 'Event was successfully created.'
+        format.html { redirect_to :back }
+      else
+        format.html { render 'new' }
+      end
+    end
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def get_event
+      @event = Event.find(params[:id])
+    end
+    
+    def get_parent_idea
+      if params[:idea_id]
+        @idea = Idea.find(params[:idea_id])
+      elsif params[:event][:idea_id]
+        @idea = Idea.find(params[:event][:idea_id])
+      elsif @event.event_id
+        @idea = Idea.find(@event.idea_id)
+      end
+      logger.info "idea is " + @idea.title
+    end
+    
+    def get_parent_relationship
+      if @idea.relationship_id
+        @relationship = Relationship.find(@idea.relationship_id)
+      end
+    end
+    
+    def event_params
+      params.require(:event).permit(:title,:description,:date,:reaction,:idea_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rak::Application.routes.draw do
-  get "events/show"
-  get "events/new"
-  get "events/edit"
   get "home/index"
   get "welcome/index"
   devise_for :users do
@@ -16,7 +13,11 @@ Rak::Application.routes.draw do
 
   resources :relationships
   
-  resources :ideas
+  resources :ideas do
+    resources :events
+  end
+  
+  resources :events
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
