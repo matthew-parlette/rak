3e7aa64519278030f9ed5958232a6e6fc212b0d2
Matt Parlette <matthew.parlette@gmail.com>
2013-11-05 11:34:46 -0500

    Show relationship dropdown on welcome page (if signed in).
    Closes #15.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+  before_filter :get_relationships
+  
   def index
   end
+  
+  private
+    def get_relationships
+      if current_user
+        @relationships = Relationship.where(:user_id => current_user[:id])
+      end
+    end
 end
